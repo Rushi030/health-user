1c5d169ecdc4c6cbed1a7587bcb38daab602ccc2
Rushi030 <rushikeshaj1@gmail.com>
2025-12-03 22:57:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 ***************************/
 
 /************* API Base URL *************/
-const API = "http://127.0.0.1:5000";
+const API = "https://health-backend-2-zs46.onrender.com";
 
 
 
@@ -1031,6 +1031,7 @@ window.onload = () => {
   }
 
 };
+
 
 
 
